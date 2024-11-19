9073212dd5d5972102e01461371c3d8f64a28adc
f.bonarota <f.bonarota@reply.it>
2024-11-19 19:12:19 +0100

    add min function

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import logging
 
-def main():
-    logging.basicConfig(filename="log/logs.log", filemode="a", format="%(levelname)s -- %(message)s", level=logging.DEBUG)
-    a = int(input("Inserisci il primo numero: "))
-    b = int(input("Inserisci il secondo numero: "))
-
+def max_value(a, b):
     if a == b:
         logging.info(f"I numeri sono identici")    
     elif a > b:
         logging.info(f"Il numero massimo tra i due: {a}")    
     else:
         logging.info(f"Il numero massimo tra i due: {b}")    
+
+def min_value(a, b):
+    if a == b:
+        logging.info(f"I numeri sono identici")    
+    elif a < b:
+        logging.info(f"Il numero minimo tra i due: {a}")    
+    else:
+        logging.info(f"Il numero minimo tra i due: {b}")    
+
+def main():
+    logging.basicConfig(filename="log/logs.log", filemode="a", format="%(levelname)s -- %(message)s", level=logging.DEBUG)
+    a = int(input("Inserisci il primo numero: "))
+    b = int(input("Inserisci il secondo numero: "))
+
+    max_value(a, b)
+    min_value(a, b)
+    
 
 
 if  __name__ == "__main__":
