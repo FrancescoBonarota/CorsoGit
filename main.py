dc62ffd52060c419a48dc502dd9a6a3f78cccca6
f.bonarota <f.bonarota@reply.it>
2024-11-27 15:55:22 +0100

    fix max function and change gitignore

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import logging
 
 def main():
-    logging.basicConfig(filename="log/logs.log", filemode="a", format="%(levelname)s -- %(message)s", level=logging.DEBUG)
+    logging.basicConfig(filename="log/logs.log", filemode="a", format="%(levelname)s -- %(message)s", level=logging.INFO)
     a = int(input("Inserisci il primo numero: "))
     b = int(input("Inserisci il secondo numero: "))
 
     if a == b:
         logging.info(f"I numeri sono identici")    
-    elif a > b:
-        logging.info(f"Il numero massimo tra i due: {a}")    
     else:
-        logging.info(f"Il numero massimo tra i due: {b}")    
-
+        max_value = max(a,b)
+        logging.info(f"Il numero massimo tra i due: {max_value}")
+  
 
 if  __name__ == "__main__":
     main()
